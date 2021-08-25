a564fe88b0678010ce5efba05edf7e11dc619155
wdk27 <phamthanhlong126@gmail.com>
2021-08-25 11:56:28 +0700

    initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Em à,',
+    introDesc: `Anh hỏi em điều này em phải trả lời thật lòng nhé!`,
+    btnIntro: 'Oke hỏi đi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên? 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy?',
+    btnYes: 'Yepppp, em thích anh lắm 🥰',
+    btnNo: 'Đồ điên!',
+    question:'Em thích anh ở điểm gì thế?',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Anh siêu siêu đẹp trai, em thích anh lắm!!!!!!',
+    mess: 'Anh biết mà 🥰. Anh cũng thích em nhiều lắm 😘',
+    messDesc: 'Quay lại nhắn tin cho anh nhá',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com/wdk27' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
